80f84a9e16396602c443fabfb9777c51e926b952
F1LT3R <al@pwn.io>
2016-11-26 21:44:23 -0500

    Pass and fail tests.

---
+++ test/fail.spec.js
@@ -0,0 +1,36 @@
+const net = require('net');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+const findPortHost = require('app/index.js');
+
+describe('test-port-host', () => {
+  it('should not find an open port when port is already used', done => {
+    const options = {
+      start: 8000,
+      end: 8100,
+      host: 'localhost'
+    };
+
+    findPortHost(options)
+    .then(port => {
+      const server = net.createServer();
+      server.listen({port, host: options.host}, () => {
+        const options2 = {
+          start: port,
+          end: port,
+          host: options.host
+        };
+
+        findPortHost(options2)
+        .catch(err => { // eslint-disable-line max-nested-callbacks
+          expect(err).to.be.a('string');
+          expect(err).to.equal(`No port found in range ${port} - ${port}`);
+          server.close();
+          done();
+        });
+      });
+    });
+  });
+});
--- test/pass.spec.js
+++ test/pass.spec.js
@@ -5,14 +5,14 @@ const expect = chai.expect;
 const findPortHost = require('app/index.js');
 
 describe('test-port-host', () => {
-  it('should find an open port between 8000 and 9000 on localhost', done => {
+  it('should find an open port between 8000 and 8100 on localhost', done => {
     const options = {
       start: 8000,
-      end: 9000,
+      end: 8100,
       host: 'localhost'
     };
 
-    findPortHost.find(options).then(port => {
+    findPortHost(options).then(port => {
       expect(port).to.be.a('number');
       expect(port).to.be.greaterThan(-1);
       done();
